42e41728b6553a162ccc36dfc5a73008e92addf3
TSRodgers <tyler.rodgers74@gmail.com>
2022-11-10 17:57:27 -0500

    fixing merge conflicts

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,51 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Photos
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown link
-            </a>
-            <div
-              className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
-          </li>
-        </ul>
-      </div>
+      
+        <Link className="navbar-brand" to='/'>
+          USS Grimes
+        </Link>
+  
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link  className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/gallery'>
+                Gallery
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/journal'>
+                Journal
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/login'>
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/signup'>
+                Signup
+              </Link>
+            </li>
+          </ul>
+        </div>
+      
     </nav>
   );
 };
